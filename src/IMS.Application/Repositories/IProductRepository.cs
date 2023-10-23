c495ee9229f3ec1e9caa9894c163e7c93bbabce4
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 13:46:06 +0330

    feat : add Create method to product repository contract

--- src/IMS.Application/Repositories/IProductRepository.cs
+++ src/IMS.Application/Repositories/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IMS.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace IMS.Application.Repositories
     public interface IProductRepository
     {
         Task<bool> IsProductTitleUnique(string title);
+        Task<Product> Create(Product product);
     }
 }
