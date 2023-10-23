58b75bb6ebea6021bd93c39141bc7c50b50825d9
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 17:54:57 +0330

    feat : add GetProductDetailQuery

---
+++ src/IMS.Application/Features/Products/Queries/GetProductDetail/GetProductDetailQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Application.Features.Products.Queries.GetProductDetail
+{
+    public sealed class GetProductDetailQuery : IRequest<ProductDetailVm>
+    {
+        public int Id { get; set; }
+    }
+}
