7a1fcdabfbfab44caf37b91d57e0dddc0f5bb5f5
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 12:16:53 +0330

    feat : add IMSException and BadRequestException

---
+++ src/IMS.Domain/Exceptions/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Domain.Exceptions
+{
+    public class BadRequestException : IMSException
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/IMS.Domain/Exceptions/IMSException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Domain.Exceptions
+{
+    public abstract class IMSException : Exception
+    {
+        protected IMSException(string message) : base(message)
+        {
+        }
+    }
+}
