672cbd5f2a99fb816361dab7d966a27508cc15b1
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 12:33:43 +0330

    feat - add order entity

---
+++ src/IMS.Domain/Entities/Order.cs
@@ -0,0 +1,36 @@
+﻿using IMS.Domain.Exceptions;
+
+namespace IMS.Domain.Entities
+{
+    public sealed class Order
+    {
+        public int Id { get; private set; }
+        public int ProductId { get; private set; }
+        public Product Product { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public int BuyerId { get; set; }
+        public User Buyer { get; set; }
+
+        public Order(int productId,int buyerId)
+        {
+            ValidateProductId(productId);
+            ValidateBuyerId(buyerId);
+
+            ProductId = productId;
+            BuyerId = buyerId;
+            CreationDate = DateTime.UtcNow;
+        }
+
+        private void ValidateProductId(int productId)
+        {
+            if (productId <= 0)
+                throw new BadRequestException("Product id is not valid");
+        }
+
+        private void ValidateBuyerId(int buyerId)
+        {
+            if (buyerId <= 0)
+                throw new BadRequestException("Buyer id is not valid");
+        }
+    }
+}
