c92d402dd3851f5cd4386d9aca071d2a7da03be8
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 18:30:36 +0330

    feat : add Create method to order service contract

---
+++ src/IMS.Application/Services/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using IMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Application.Services
+{
+    public interface IOrderService
+    {
+        Task<Order> Create(Order order);
+    }
+}
