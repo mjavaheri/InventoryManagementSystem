e54730c8a5c91184d67de296f58c8467cd195a65
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 20:31:04 +0330

    feat : add InfrastructureServiceRegistration

---
+++ src/IMS.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,32 @@
+﻿using IMS.Application.Repositories;
+using IMS.Application.Services;
+using IMS.Infrastructure.EF;
+using IMS.Infrastructure.Repositories;
+using IMS.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<IMSDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("IMSConnectionString")));
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddScoped<IOrderService, OrderService>();
+
+            return services;
+        }
+    }
+}
