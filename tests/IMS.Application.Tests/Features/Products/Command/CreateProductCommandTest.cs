b43e1571aa5a16cde9fce1caac1537fb64379435
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 23:42:25 +0330

    feat : add ShouldThrowValidationExceptionWhenTitleIsNotUnique test to CreateProductCommandTest

--- tests/IMS.Application.Tests/Features/Products/Command/CreateProductCommandTest.cs
+++ tests/IMS.Application.Tests/Features/Products/Command/CreateProductCommandTest.cs
@@ -37,5 +37,30 @@ namespace IMS.Application.Tests.Features.Products.Command
 
             Assert.Equal("Title must not exceed 40 characters.", exception.ValdationErrors.First());
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionWhenTitleIsNotUnique()
+        {
+            //Arrange
+            var mockRepository = new Mock<IProductRepository>();
+
+            mockRepository.Setup(x => x.IsProductTitleUnique(It.IsAny<string>())).Returns(Task.FromResult(true));
+
+            var command = new CreateProductCommand()
+            {
+                Title = "Vivobook 16 R1605ZA",
+                Price = 1000.0M,
+                Discount = 20
+            };
+
+            var handler = new CreateProductCommandHandler(mockRepository.Object);
+
+            var exception = await Assert.ThrowsAsync<IMSValidationException>(async () =>
+            {
+                await handler.Handle(command, CancellationToken.None);
+            });
+
+            Assert.Equal("Product's title already exists.", exception.ValdationErrors.First());
+        }
     }
 }
