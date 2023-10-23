ec6fbc8d0882b43a165bb2cdd2be441a9d947bba
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 20:02:34 +0330

    feat : add user repository

---
+++ src/IMS.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using IMS.Application.Repositories;
+using IMS.Infrastructure.EF;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IMSDbContext _dbContext;
+
+        public UserRepository(IMSDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> IsUserExists(int userId)
+        {
+            return await _dbContext.Users.AnyAsync(x => x.Id == userId);
+        }
+    }
+}
