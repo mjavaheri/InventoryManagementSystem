598e0c8a2984c56629cc427aea3aca4a97d0f1a2
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 13:39:34 +0330

    feat : add IMSValidationException

---
+++ src/IMS.Application/Exceptions/IMSValidationException.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.Results;
+using IMS.Domain.Exceptions;
+
+namespace IMS.Application.Exceptions
+{
+    public sealed class IMSValidationException : IMSException
+    {
+        public List<string> ValdationErrors { get; private set; }
+
+        public IMSValidationException(ValidationResult validationResult, string message) : base(message)
+        {
+            ValdationErrors = new List<string>();
+
+            foreach (var validationError in validationResult.Errors)
+            {
+                ValdationErrors.Add(validationError.ErrorMessage);
+            }
+        }
+    }
+}
