2ed77610f8fbac90a0fdb56fc4f20143f93abc81
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 12:23:37 +0330

    feat : add user entity

---
+++ src/IMS.Domain/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using IMS.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Domain.Entities
+{
+    public sealed class User
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public User(string name)
+        {
+            ValidateName(name);
+
+            Name = name;
+        }
+
+        private void ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new BadRequestException("User's name can not be null or whitespace");
+        }
+    }
+}
