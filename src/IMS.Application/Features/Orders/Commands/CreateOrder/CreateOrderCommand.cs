15cef86da91112cc06fd994f071934dccab148ad
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 18:22:24 +0330

    feat : add CreateOrderCommand

---
+++ src/IMS.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Application.Features.Orders.Commands.CreateOrder
+{
+    public sealed class CreateOrderCommand : IRequest<int>
+    {
+        public int ProductId { get; set; }
+        public int BuyerId { get; set; }
+    }
+}
