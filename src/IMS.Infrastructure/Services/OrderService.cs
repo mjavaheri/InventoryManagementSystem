fdf753c07816959fe4525a2e529a997e040b1cec
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 20:03:21 +0330

    feat : add order service

---
+++ src/IMS.Infrastructure/Services/OrderService.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Core;
+using IMS.Application.Services;
+using IMS.Domain.Entities;
+using IMS.Domain.Exceptions;
+using IMS.Infrastructure.EF;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace IMS.Infrastructure.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IMSDbContext _dbContext;
+
+        public OrderService(IMSDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Order> Create(Order order)
+        {
+            using var transaction = _dbContext.Database.BeginTransaction(IsolationLevel.RepeatableRead);
+
+            try
+            {
+                var product = _dbContext.Products.FirstOrDefault(x => x.Id == order.ProductId);
+
+                if (product == null)
+                { 
+                   throw new BadRequestException($"Product {order.Id} is not valid");
+                }
+
+                product.DecreaseInventoryCount();
+
+                _dbContext.Orders.Add(order);
+
+                _dbContext.Entry(product).State = EntityState.Modified;
+
+                await _dbContext.SaveChangesAsync();
+
+                await transaction.CommitAsync();
+
+                return order;
+
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw;
+            }
+        }
+    }
+}
