2a6b0054a749e2864c266d2ee72d1cb3df547418
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 15:45:46 +0330

    feat : add IMSNotFoundException

---
+++ src/IMS.Application/Exceptions/IMSNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using IMS.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Application.Exceptions
+{
+    public sealed class IMSNotFoundException : IMSException
+    {
+        public IMSNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
