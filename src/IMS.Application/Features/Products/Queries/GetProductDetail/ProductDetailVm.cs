abe12e76cafa948651a896b0807a4e65e1c85939
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 17:54:19 +0330

    feat : add ProductDetailVm

---
+++ src/IMS.Application/Features/Products/Queries/GetProductDetail/ProductDetailVm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Application.Features.Products.Queries.GetProductDetail
+{
+    public sealed class ProductDetailVm
+    {
+        private readonly decimal _price;
+        private readonly byte _discount;
+        public ProductDetailVm(decimal price,byte discount) 
+        { 
+            _price = price;
+            _discount = discount;
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price 
+        {
+            get
+            {
+                decimal discountValue = _price * ( _discount / 100m);
+                decimal priceAfterDiscount = _price - discountValue;
+                return  priceAfterDiscount;
+            }
+        }
+        public byte[] RowVersion { get; set; }
+    }
+}
