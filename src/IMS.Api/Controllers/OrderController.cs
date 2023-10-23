b12cd253427c377506ea4374cde87fe7e026f339
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 20:32:35 +0330

    feat : add order controller

---
+++ src/IMS.Api/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using IMS.Application.Features.Orders.Commands.CreateOrder;
+using IMS.Application.Features.Products.Commands.CreateProduct;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMS.Api.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CreateOrderCommand createCommand)
+        {
+            var id = await _mediator.Send(createCommand);
+            return Ok(new { id });
+        }
+    }
+}
