4b1cab939a70574ce0f92eb92b7fda4d1d790732
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-24 00:05:21 +0330

    feat : add ShuoldHitRepositoryWhenProductIsNotInCacheAndIncreaseCacheCount test to GetProductDetailQueryTest

--- tests/IMS.Application.Tests/Features/Products/Query/GetProductDetailQueryTest.cs
+++ tests/IMS.Application.Tests/Features/Products/Query/GetProductDetailQueryTest.cs
@@ -33,6 +33,34 @@ namespace IMS.Application.Tests.Features.Products.Query
             mockRepository.Verify(x => x.GetById(It.IsAny<int>()), Times.Never);
         }
 
+        [Fact]
+        public async Task ShuoldHitRepositoryWhenProductIsNotInCacheAndIncreaseCacheCount()
+        {
+            // Arrange
+            var mockRepository = new Mock<IProductRepository>();
+
+            mockRepository.Setup(x => x.GetById(It.IsAny<int>())).Returns(Task.FromResult(new Product("Vivobook 16 R1605ZA", 1000, 10)));
+
+            var command = new GetProductDetailQuery()
+            {
+                Id = 1
+            };
+
+            var cache = new MemoryCache(new MemoryCacheOptions());
+
+            var handler = new GetProductDetailQueryHandler(mockRepository.Object, cache);
+
+            // Act            
+            var result = await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.Equal("Vivobook 16 R1605ZA", result.Title);
+
+            mockRepository.Verify(x => x.GetById(It.IsAny<int>()), Times.Once);
+
+            Assert.Equal(1, cache.Count);
+        }
+
         private static Product GetCachedProduct()
         {
             return new Product("Vivobook 16 R1605ZA", 1000, 10);
