e3d3ba786c3a9375375a33d2fa0fb7c5bfa595e9
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 15:21:09 +0330

    feat : add UpdateInventoryCountCommandValidator

---
+++ src/IMS.Application/Features/Products/Commands/UpdateInventoryCount/UpdateInventoryCountCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Application.Features.Products.Commands.UpdateInventoryCount
+{
+    public sealed class UpdateInventoryCountCommandValidator : AbstractValidator<UpdateInventoryCountCommand>
+    {
+        public UpdateInventoryCountCommandValidator() 
+        {
+            RuleFor(p => p.Value)
+                .NotEmpty().WithMessage("{PropertyName} is required.")
+                .GreaterThanOrEqualTo(0);
+
+            RuleFor(p => p.RowVersion)
+                .NotEmpty().WithMessage("{PropertyName} is required.");
+        }
+    }
+}
