462c35679fab872a50c31ff4f1786f383fda99f9
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 19:12:22 +0330

    feat : add IMSDbContext

---
+++ src/IMS.Infrastructure/EF/IMSDbContext.cs
@@ -0,0 +1,27 @@
+﻿using IMS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Infrastructure.EF
+{
+    public class IMSDbContext : DbContext
+    {
+        public IMSDbContext(DbContextOptions<IMSDbContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(IMSDbContext).Assembly);
+        }
+    }
+}
