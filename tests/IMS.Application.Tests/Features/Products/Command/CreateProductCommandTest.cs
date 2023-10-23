e7953d19d25e50925c57d6f6c18e8cf576d91ede
Mehdi Javaheri <megdijavaheri@hotmail.com>
2023-10-23 23:34:04 +0330

    feat : add ShouldThrowValidationExceptionWhenTitleIsGreaterThan40 test to CreateProductCommandTest

---
+++ tests/IMS.Application.Tests/Features/Products/Command/CreateProductCommandTest.cs
@@ -0,0 +1,41 @@
+﻿using IMS.Application.Exceptions;
+using IMS.Application.Features.Products.Commands.CreateProduct;
+using IMS.Application.Repositories;
+using IMS.Domain.Entities;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Application.Tests.Features.Products.Command
+{
+    public class CreateProductCommandTest
+    {
+
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionWhenTitleIsGreaterThan40()
+        {
+            //Arrange
+            var mockRepository = new Mock<IProductRepository>();
+
+            var command = new CreateProductCommand()
+            {
+                Title = "Vivobook 16 R1605ZA - Vivobook 16 R1605ZA - Vivobook 16 R1605ZA - Vivobook 16 R1605ZA",
+                Price = 1000.0M,
+                Discount = 20
+            };
+
+            var handler=new CreateProductCommandHandler(mockRepository.Object);
+
+            var exception = await Assert.ThrowsAsync<IMSValidationException>(async () =>
+            {
+                await handler.Handle(command, CancellationToken.None);
+            }); 
+
+            Assert.Equal("Title must not exceed 40 characters.", exception.ValdationErrors.First());
+        }
+    }
+}
